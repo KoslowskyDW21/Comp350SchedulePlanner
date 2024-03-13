49cae9a94d672d6bb9dad436e6945c6fb12fd806
abigail-kiser <abigail_kiser@hotmail.com>
2024-03-13 14:15:12 -0400

    search progress, added static database to course

--- src/main/java/edu/gcc/comp350/smartest/Search.java
+++ src/main/java/edu/gcc/comp350/smartest/Search.java
@@ -1,25 +1,37 @@
 package edu.gcc.comp350.smartest;
-
 import java.util.ArrayList;
+
 public class Search {
     private String userInput;
     private ArrayList<Course> results;
+    private ArrayList<Filter> activeFilters;
 
     public Search() {
         this.userInput = "";
         this.results = new ArrayList<Course>();
+        this.activeFilters = new ArrayList<>();
     }
 
-    public ArrayList<Course> modifyQuery(String input) { return null; }
-
-    public ArrayList<Course> addFilter() {
+    public ArrayList<Course> modifyQuery(String input) {
+        userInput = input;
+        parseDatabase();
         return null;
     }
 
-    public ArrayList<Course> removeFilter(){
+    public ArrayList<Course> addFilter(Filter filter) {
+        activeFilters.add(filter);
+        parseDatabase();
         return null;
     }
 
+    public ArrayList<Course> removeFilter(Filter filter){
+        activeFilters.remove(filter);
+        parseDatabase();
+        return null;
+    }
 
+    public ArrayList<Course> parseDatabase() {
+        return null;
+    }
 
 }
