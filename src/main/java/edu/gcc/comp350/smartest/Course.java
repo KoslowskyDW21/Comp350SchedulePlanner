21d158be5684ae304d8ce64c756a6a65ece8a049
KoslowskyDW21 <KoslowskyDW21@gcc.edu>
2024-03-13 17:08:33 -0400

    Parse the course file and add all of the courses to a list in main

--- src/main/java/edu/gcc/comp350/smartest/Course.java
+++ src/main/java/edu/gcc/comp350/smartest/Course.java
@@ -1,45 +1,72 @@
 package edu.gcc.comp350.smartest;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-// hi guys!!
 public class Course {
     private final String courseCode;
     private final String name;
     private final String professor;
     private String color;
-    //private final LocalTime[] timeSlot;
-    private final int[] startTimes;
+    private final String startTime;
+    private final String endTime;
     private final String description;
     private final int numCredits;
-    private final Course[] preReqs;
-    private final Course[] coReqs;
+    private final ArrayList<Course[]> preReqs;
+    private final ArrayList<Course[]> coReqs;
+    private final int numSeats;
+    private final String days;
+    private final String semester;
 
-
-    /*public LocalTime[] getTimeSlot() {
-        return timeSlot;
-    }*/
-
-    public Course[] getPreReqs() {
+    public ArrayList<Course[]> getPreReqs() {
         return preReqs;
     }
 
-    public Course[] getCoReqs() {
+    public ArrayList<Course[]> getCoReqs() {
         return coReqs;
     }
 
-    public Course(String courseCode, String name, String professor, String color, LocalTime[] timeSlot,
-                  String description, int numCredits, Course[] preReqs, Course[]coReqs) {
-        this.courseCode = courseCode;
-        this.name = name;
-        this.professor = professor;
-        this.color = color;
-        //this.timeSlot = timeSlot;
-        this.description = description;
-        this.numCredits = numCredits;
-        this.preReqs = preReqs;
-        this.coReqs = coReqs;
-        startTimes = new int[0];
+//    public Course(String courseCode, String name, String professor, String color, String description,
+//                  int numCredits, ArrayList<Course[]> preReqs, ArrayList<Course[]> coReqs,
+//                  String startTime, String endTime, int numSeats, String days) {
+//        this.courseCode = courseCode;
+//        this.name = name;
+//        this.professor = professor;
+//        this.color = color;
+//        this.description = description;
+//        this.numCredits = numCredits;
+//        this.preReqs = preReqs;
+//        this.coReqs = coReqs;
+//        this.startTime = startTime;
+//        this.endTime = endTime;
+//        this.numSeats = numSeats;
+//        this.days = days;
+//    }
+
+    public Course(String excelLine) {
+        String[] tokens = excelLine.split(",");
+        courseCode = tokens[2] + tokens[3] + tokens[4];
+        name = tokens[5];
+        professor = tokens[17] + " " + tokens[16];
+        color = "white";
+        if(tokens.length >= 20) {
+            description = tokens[19];
+        } else {
+            description = "";
+        }
+        numCredits = Integer.parseInt(tokens[6]);
+        numSeats = Integer.parseInt(tokens[7]);
+        preReqs = new ArrayList<>();
+        coReqs = new ArrayList<>();
+        startTime = tokens[14];
+        endTime = tokens[15];
+        days = tokens[9] + tokens[10] + tokens[11] + tokens[12] + tokens[13];
+        if(Integer.parseInt(tokens[1]) == 10) {
+            semester = "Fall";
+        } else { //tokens[5] == 30
+            semester = "Spring";
+        }
     }
 
     private void changeColor(String color) {
--- src/main/java/edu/gcc/comp350/smartest/Main.java
+++ src/main/java/edu/gcc/comp350/smartest/Main.java
@@ -1,7 +1,30 @@
 package edu.gcc.comp350.smartest;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
+    public static ArrayList<Course> courseList;
+
     public static void main(String[] args) {
-        System.out.println("Never gonna give you up");
+        courseList = new ArrayList<>();
+
+        try {
+            ParseClasses();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e);
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+    }
+
+    private static void ParseClasses() throws IOException {
+        Scanner scn = new Scanner(new File("2020-2021.csv"));
+        scn.nextLine();
+        while(scn.hasNext()) {
+            courseList.add(new Course(scn.nextLine()));
+        }
     }
 }
