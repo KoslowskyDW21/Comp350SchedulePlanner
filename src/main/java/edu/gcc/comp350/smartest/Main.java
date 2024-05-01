1ff25c94cd3cf603179840f3bd0ae17be24b74d6
Jeffrey Krug <krugjl21@gcc.edu>
2024-05-01 13:53:03 -0400

    I did a bunch of stuff. This will cause merge conflicts, but it should be okay.

--- src/main/java/edu/gcc/comp350/smartest/Main.java
+++ src/main/java/edu/gcc/comp350/smartest/Main.java
@@ -1,24 +1,21 @@
 package edu.gcc.comp350.smartest;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.Scanner;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Parent root = FXMLLoader.load(getClass().getResource("/home.fxml"));
+        Scene scene = new Scene(root, 800, 500);
         stage.setScene(scene);
         stage.show();
     }
@@ -29,6 +26,10 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch();
         // Create the course database
+        //setUp();
+    }
+
+    public static void setUp() {
         try {
             Search.ParseClasses();
         } catch (FileNotFoundException e) {
